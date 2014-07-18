fe25bb6cde2b823cb8ecd5b0c61fafe7e6ce8efb
Jose <jose@zunware.com>
2014-07-18 18:26:29 -0500

    Added some comments for the reviewers enjoyment.

--- cap-reader.php
+++ cap-reader.php
@@ -1,12 +1,19 @@
 <?php
 
+// Bienvenido a nuestro código =)
+// Creado para Nibsi por jose@josellausas en Julio 2014
+
+// Clase que encapsula nuestra herramienta de parsing de CAP
 class LL_CAP
 {
+
+	// Descarga el contenido de un URL
 	private function downloadXML($url)
 	{
 		return file_get_contents($url);
 	}
 	
+	// Devuelve el contenido XML en un objeto PHP
 	public function get_cap( $url )
 	{
 		$xml = simplexml_load_string($this->downloadXML($url));	
@@ -15,19 +22,26 @@ class LL_CAP
 	}
 	
 
+	// Devuelve un objeto más limpio y con nuestro propio formato.
 	public function get( $url )
 	{
 		$xml = simplexml_load_string($this->downloadXML($url));
 
+		// Una nueva clase para guardar los datos interesantes.
 		$myCap = new stdClass();
+
+
 		$myCap->title 	= $xml->title;
 		$myCap->updated = $xml->updated;
 
+		// Aqui se guardan todas las alertas
 		$myCap->alertas = array();
 
 
+		// Navegar el arbol XML y obtener los datos interesantes.
 		foreach ($xml->entry as $entry)
 		{
+			// Almacena todos los datos pertinentes a una instancia de alerta
 			$la_alerta = new stdclass();
 
 			$la_alerta->title 	= $entry->title;
@@ -50,6 +64,7 @@ class LL_CAP
 
 						$la_alerta->areas = array();
 
+						// El entry puede tener mas de un area.
 						foreach ($info->area as $area)
 						{
 							$my_area = new stdClass();
@@ -68,10 +83,6 @@ class LL_CAP
 
 							array_push($la_alerta->areas, $my_area);
 						}
-
-						// echo "\t\tEvent    = " . $info->event       . "\n";
-						// echo "\t\tHeadline = " . $info->headline    . "\n";
-						// echo "\t\tDesc     = " . $info->description . "\n\n";
 					}
 				}
 			}
